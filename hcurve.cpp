b2c342bdac67f5633b2efa03a1c404a96b6137be
Lrapava <46052668+Lrapava@users.noreply.github.com>
2022-05-14 17:13:46 +0400

    Update hcurve.cpp

--- hcurve.cpp
+++ hcurve.cpp
@@ -151,7 +151,7 @@ int main(int argc, char** argv) {
 
 void help(int x) {
 	cout 
-	<< "Lrapava/hcurve is a simple command-line utility for generating pseudo Hilbert curves' visualisations.\n"
+	<< "Lrapava/hcurve is a simple command-line utility for generating pseudo Hilbert curves' visualizations.\n"
 	<< "You can view source-code at https://github.com/Lrapava/hcurve\n"
 	<< "For more informatiob about Hilbert curve view https://youtu.be/3s7h2MHQtxc\n\n"
 	<< "Usage:\n"
